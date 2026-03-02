e3064ea438aecb8374ef58585b78fd0fd693b00f
Tamara <tamaradjurcik@gmail.com>
2026-03-02 22:59:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Smooth scroll
+document
+    .querySelectorAll('nav a[href^="#"], .mobile-menu a[href^="#"]')
+    .forEach((anchor) => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50,
+                    behavior: 'smooth',
+                });
+            }
+        });
+    });
